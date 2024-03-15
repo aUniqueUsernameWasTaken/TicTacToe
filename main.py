5d61170de571bf4093caa704013d23640db7b1ca
aunqiueusernamewastaken <yalo@archer.localdomain>
2024-03-15 14:49:02 +0000

    actually coded it

---
+++ main.py
@@ -0,0 +1,131 @@
+import random
+import sys
+
+possibleNumbers = [1,2,3,4,5,6,7,8,9]
+
+print('Welcome to Tic Tac Toe')
+print(20*'-')
+
+gameBoard =[[1,2,3],[4,5,6],[7,8,9]]
+rows = 3
+columns = 3
+
+def printGameBoard():
+    for x in range(rows):
+        print("\n+---+---+---+")
+        print("|",end="")
+        for y in range(columns):
+            print("", gameBoard[x][y], end=" |")
+    print("\n+---+---+---+")
+
+def modifyArray(num, turn):
+    num -= 1
+    if num == 0:
+        gameBoard[0][0] = turn
+    elif num == 1:
+        gameBoard[0][1] = turn
+    elif num == 2:
+        gameBoard[0][2] = turn
+    elif num == 3:
+        gameBoard[1][0] = turn
+    elif num == 4:
+        gameBoard[1][1] = turn
+    elif num == 5:
+        gameBoard[1][2] = turn
+    elif num == 6:
+        gameBoard[2][0] = turn
+    elif num == 7:
+        gameBoard[2][1] = turn
+    elif num == 8:
+        gameBoard[2][2] = turn
+
+def winnerWinnerChickenDinner(gameBoard):
+    # x 
+    if gameBoard[0][0] == 'X' and gameBoard[0][1] == 'X' and gameBoard[0][2] == 'X':
+        print('X has won!')
+        return 'X'
+    elif gameBoard[0][0] == 'O' and gameBoard[0][1] == 'O' and gameBoard[0][2] == 'O':
+        print('O has won!')
+        return 'O'
+    
+    elif gameBoard[1][0] == 'X' and gameBoard[1][1] == 'X' and gameBoard[1][2] == 'X':
+        print('X has won!')
+        return 'X'
+    elif gameBoard[1][0] == 'O' and gameBoard[1][1] == 'O' and gameBoard[1][2] == 'O':
+        print('O has won!')
+        return 'O'
+    
+    elif gameBoard[2][0] == 'X' and gameBoard[2][1] == 'X' and gameBoard[2][2] == 'X':
+        print('X has won!')
+        return 'X'
+    elif gameBoard[2][0] == 'O' and gameBoard[2][1] == 'O' and gameBoard[2][2] == 'O':
+        print('O has won!')
+        return 'O'
+
+
+    # y
+    elif gameBoard[0][0] == 'X' and gameBoard[1][0] == 'X' and gameBoard[2][0] == 'X':
+        print('X has won!')
+        return 'X'
+    elif gameBoard[0][0] == 'O' and gameBoard[1][0] == 'O' and gameBoard[2][0] == 'O':
+        print('O has won!')
+        return 'O'
+    
+    elif gameBoard[0][1] == 'X' and gameBoard[1][1] == 'X' and gameBoard[2][1] == 'X':
+        print('X has won!')
+        return 'X'
+    elif gameBoard[0][1] == 'O' and gameBoard[1][1] == 'O' and gameBoard[2][1] == 'O':
+        print('O has won!')
+        return 'O'
+    
+    elif gameBoard[0][2] == 'X' and gameBoard[1][2] == 'X' and gameBoard[2][2] == 'X':
+        print('X has won!')
+        return 'X'
+    elif gameBoard[0][2] == 'O' and gameBoard[1][2] == 'O' and gameBoard[2][2] == 'O':
+        print('O has won!')
+        return 'O'
+
+
+
+    # crossways
+    elif gameBoard[0][0] == 'X' and gameBoard[1][1] == 'X' and gameBoard[2][2] == 'X':
+        print('X has won!')
+        return 'X'
+    elif gameBoard[0][0] == 'O' and gameBoard[1][1] == 'O' and gameBoard[2][2] == 'O':
+        print('O has won!')
+        return 'O'  
+    elif gameBoard[2][0] == 'X' and gameBoard[1][1] == 'X' and gameBoard[0][2] == 'X':
+        print('X has won!')
+        return 'X'
+    elif gameBoard[2][0] == 'O' and gameBoard[1][1] == 'O' and gameBoard[0][2] == 'O':
+        print('O has won!')
+        return 'O'
+
+leaveLoop = False
+turn = 'X'
+turnCounter = 0
+
+while leaveLoop == False:
+    if winnerWinnerChickenDinner(gameBoard) == 'X' or winnerWinnerChickenDinner(gameBoard) == 'O':
+        sys.exit()
+    # Our Turn
+    if turnCounter % 2 == 1:
+        printGameBoard()
+        numberPicked = int(input('\nChoose a number [1-9]: '))
+        if numberPicked >= 1 or numberPicked <= 9:
+            modifyArray(numberPicked, turn)
+            possibleNumbers.remove(numberPicked)
+        else:
+            print("Invalid Input, enter again.")
+        turnCounter += 1
+    # PC's Turn    
+    else:
+        while True:
+            cpuChoice = random.choice(possibleNumbers)
+            print("\nPC Choice: ", cpuChoice)
+            if cpuChoice in possibleNumbers:
+                modifyArray(cpuChoice, 'O')
+                possibleNumbers.remove(cpuChoice)
+                turnCounter += 1
+                break
+
